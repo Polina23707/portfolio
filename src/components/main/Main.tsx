eced127a759d098dae3c77c3da9c5c790390ca65
Polina Gorelik <Polina23707@gmail.com>
2024-02-26 19:50:49 +0200

    add State in Main.tsx

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,14 +1,37 @@
 import About from "../about/About";
 import './Main.css'
 import Projects from "../projects/Projects";
-import Contact from "../contact/Contact";
+import { useState } from "react";
+import ProjectPage from "../projectPage/ProjectPage";
 
 const Main = () => {
+  const [project, setProject] = useState('');
+
+  const onClick = (e: any) => {
+    setProject(e.target.id);
+  }
+
+  const checkState = () => {
+    if (project === '') {
+      return(
+        <div className="main">
+          <About />
+          <Projects onClick={onClick}/>
+      </div>
+      )
+
+    } else {
+      return(
+        <div className="main">
+          <ProjectPage id={project} onClick={onClick}/>
+        </div>
+      )
+    }
+  }
+
   return(
-    <div className="main">
-      <About />
-      <Projects />
-      <Contact />
+    <div>
+      {checkState()}
     </div>
   )
 }
