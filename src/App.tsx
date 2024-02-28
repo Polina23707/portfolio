4c9ad1b5d89cc88db308016cba0a887be184e27d
Polina Gorelik <Polina23707@gmail.com>
2024-02-28 19:46:13 +0200

    moved setProject & setMobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,32 @@ import './App.css'
 import Main from './components/main/Main'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
+import { useState } from 'react'
 
 function App() {
+  const [project, setProject] = useState('');
+
+  function onMenuItemClick() {
+    setProject('');
+    if (mobile === 'flex') {
+      setMobile('none');
+    } 
+  }
+
+  const onClick = (e: any) => {
+    setProject(e.target.id);
+  }
+
+  const [mobile, setMobile] = useState('none');
+
+  function onMenu() {
+    (mobile === 'none') ? setMobile('flex'): setMobile('none');    
+  }
+
   return (
     <>
-      <Header />
-      <Main />
+      <Header onMenuItemClick={onMenuItemClick} mobile={mobile} onMenu={onMenu}/>
+      <Main onClick={onClick} project={project}/>
       <Footer />
     </>
   )
